12c978e8b23a1dda44115a49d94df019e5d9dee5
Speedfire667 <mateusportelag1@gmail.com>
2024-10-03 11:20:06 -0300

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,8 +4,8 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
+    host: "Speedfire667.aternos.me",
+    port: "56479",
     username: "24ATERNOSBOT",
     version: false
     })
